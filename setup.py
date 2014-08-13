d51cb7329d03dc0cd698fcd67c1a5daf965c1baf
Florian N <michael@hostechs.com>
2014-08-12 22:48:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     license='MIT',
     description='A reusable django app for monitoring your linux server.',
-    long_description='',
+    long_description=README,
     url='https://github.com/k3oni/pydash-django-app',
     author='Florian N.',
     author_email='michaelneagu@gmail.com',
